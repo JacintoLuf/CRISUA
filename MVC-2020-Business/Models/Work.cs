eacd20c8f14b6d842ad7be830daa36766dda92e7
SamuelRDuarte <samuelduarte10@ua.pt>
2020-05-12 19:05:07 +0100

    pagina de importação

--- MVC-2020-Business/Models/Work.cs
+++ MVC-2020-Business/Models/Work.cs
@@ -325,14 +325,14 @@ namespace MVC_2020_Business.Models
         [JsonProperty("contributor")]
         public List<Contributor> contributor { get; set; }
 
-        public string ToString(string year, string sourceName)
+        public string ToString(string year)
         {
             var ListToReturn = new List<string>();
             foreach (var item in contributor)
             {
                 ListToReturn.Add(item.creditName.value);
             }
-            return "Contributors: " + string.Join("; ", ListToReturn) + Environment.NewLine + " || Year: " + year + Environment.NewLine + " || SourceName: " + sourceName;
+            return "Contributors: " + string.Join("; ", ListToReturn) + "\n Year: " + year;
         }
     }
 
