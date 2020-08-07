902a0d424ac99c493d7a588f8282775f8c91beb1
SamuelRDuarte <samuelduarte10@ua.pt>
2020-08-07 15:43:25 +0100

    criação das classes para a api

---
+++ MVC-2020-Business/Models/Autor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_2020_Business.Models
+{
+    public class Autor
+    {
+        public string Name { get; set; }
+
+        public string OrcidId { get; set; }
+
+        public string Iupi { get; set; } //não sei se é preciso
+    }
+}
